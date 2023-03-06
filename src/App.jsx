0ff3c5ed393cf4049f0757f3e7f5762acde351d2
Adarsh Kumar <adarsh010c@gmail.com>
2023-03-06 17:28:49 +0530

    Added track state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export default function App(){
+
+    const [ track , setTrack ] = React.useState('')
+    console.log( track )
+    const handleChange = ( event ) => {
+        setTrack( event.target.value )
+    }
+
     return(
         <div>
             <h1>Speed + Typing</h1>
             <textarea
-                defaultValue='Initial value'
+                name='track'
+                value={track}
+                onChange={handleChange}
             />
             <h4>Time Remaining: 00:00</h4>
             <button>Start</button>
