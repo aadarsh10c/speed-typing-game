261b84c33a8c253c5010e3a8438048525dd32e24
Adarsh Kumar <adarsh010c@gmail.com>
2023-03-06 17:53:42 +0530

    Added count state and functionality in APP component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 export default function App(){
 
-    const [ track , setTrack ] = React.useState('')
-    console.log( track )
+    const [ track , setTrack ] = useState('')
+    const [ count , setCount ] = useState( 0 )
+
     const handleChange = ( event ) => {
         setTrack( event.target.value )
+    }
+
+    const calculateWords = ( text ) => {
+        let words = text.trim().split( " " )
+        let filteredWordCount = words.filter( value => value != '').length
+        setCount( filteredWordCount )
     }
 
     return(
@@ -17,8 +24,10 @@ export default function App(){
                 onChange={handleChange}
             />
             <h4>Time Remaining: 00:00</h4>
-            <button>Start</button>
-            <h1>Word Count : 0</h1>
+            <button 
+                onClick={ () => calculateWords( track ) }
+                >Start</button>
+            <h1>Word Count : {count}</h1>
         </div>
         
     )
